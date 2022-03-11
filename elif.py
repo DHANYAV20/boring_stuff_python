09648f213daa5233d0a7df37ddb3158b77e95236
DHANYAV20 <dhanya1509@gmail.com>
2022-03-11 21:23:36 +0530

    elif statement

---
+++ elif.py
@@ -0,0 +1,10 @@
+name="Nakhul"
+age=35
+if name == "hansika":
+    print("Hi Nakhul")
+elif name == "Nakhul":
+    print("Hi Nakhul")
+if age > 200:
+    print("Your age is 35")
+elif age == 35:
+    print("Your age is 35")
